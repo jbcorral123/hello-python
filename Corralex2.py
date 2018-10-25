b538cf0968be97ef6695df299176d960231789ef
jbcorral123 <44455505+jbcorral123@users.noreply.github.com>
2018-10-25 17:02:51 +0800

    Add files via upload

---
+++ Corralex2.py
@@ -0,0 +1,10 @@
+if x>= 220:
+    print(x, "Super Typhoon")
+elif x=<220 or x==180:
+    print(x, "Typhoon")
+elif x=<,117 or x==89:
+    print(x, "Several Tropical Typhoon")
+elif x=<88 or x==62:
+    print(x, "Tropical Storm")
+else x=<61:
+    print(x, "Tropical Depression")

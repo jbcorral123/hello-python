6a35869b630679af6476cbef123b38104a2be5b0
jbcorral123 <44455505+jbcorral123@users.noreply.github.com>
2018-10-25 15:09:47 +0800

    Add files via upload

---
+++ Corral.py
@@ -0,0 +1,5 @@
+print("I am Joy Beth Corral")
+print("My spirit name is Dragon, ")
+print("because I am born in the year 2000 and its strong")
+print("When not in school", "I love to play guitar")
+print("I dream of of being an engineer in the future")

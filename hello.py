e164180c95e299e7ce7aa84e8089e7e13f207203
jbcorral123 <44455505+jbcorral123@users.noreply.github.com>
2018-10-26 09:38:34 +0800

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,2 +1,4 @@
 print("Hello, world")
-print("My name is {}. I am {} y/o.") .format("Cor", 16))
+print("My name is{}. I am {} y/o." .format("Cor",16)
+print("Adnu", 2018, sep="-", end="")
+print("CSNHS") 
